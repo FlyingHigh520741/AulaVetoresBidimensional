e1d92c1bff22e0d8aafa12ffbc9a2fad8f37d957
DANIEL VIEIRA NOVAIS <daniel.vnovais@senacsp.edu.br>
2025-11-27 21:45:35 -0300

    Matriz de cadastro em c#

--- VetoresBidimensionais/VetoresBidimensionais/Program.cs
+++ VetoresBidimensionais/VetoresBidimensionais/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[,] agenda;
+            int i;
+
+            agenda = new string[3, 3];
+
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Faça o {i + 1}º cadastro: \n\n");
+                Console.Write($"Digite o nome da {i + 1}ª pessoa: ");
+                agenda[i, 0] = Console.ReadLine();
+                Console.Write($"Digite o cpf da {i + 1}ª pessoa: ");
+                agenda[i, 1] = Console.ReadLine();
+                Console.Write($"Digite o bairro da {i + 1}ª pessoa: ");
+                agenda[i, 2] = Console.ReadLine();
+                Console.WriteLine("\n\n");
+
+            }
+
+            for (i = 0; i < 3; i++)
+            {
+                
+                Console.WriteLine($"-----Cadastro {i}-----");
+                Console.WriteLine(agenda[i, 0]);
+                Console.WriteLine(agenda[i, 1]);
+                Console.WriteLine(agenda[i, 2]);
+                Console.WriteLine("\n\n");
+            }
         }
-    }
+
+        }
 }
